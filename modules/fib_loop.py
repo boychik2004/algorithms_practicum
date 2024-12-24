0cc151d161d7815aeb1babec0f4ed9cf38b9a631
MaxX <120455884+boychik2004@users.noreply.github.com>
2024-12-24 22:05:39 +0300

    Update fib_loop.py

--- modules/fib_loop.py
+++ modules/fib_loop.py
@@ -1,4 +1,5 @@
 import time
+
 def fib(n):
     if n == 0:
         return 0
@@ -12,11 +13,11 @@ def fib(n):
 def measure_execution_time():
     test_values = [5, 10, 15, 20, 32]
     for n in test_values:
-        start_time = time.time()
+        start_time = time.perf_counter()
         result = fib(n)
-        end_time = time.time()
-        elapsed_time = (end_time - start_time) * 1000
-        print(f"fib({n}) = {result}, time: {elapsed_time:.2f} ms")
+        end_time = time.perf_counter()
+        elapsed_time = (end_time - start_time) * 1000  # Перевод в миллисекунды
+        print(f"fib({n}) = {result}, time: {elapsed_time:.3f} ms")  # Три знака после запятой
 
 if __name__ == "__main__":
     measure_execution_time()
